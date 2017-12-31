599c5e1a1126615a49276f55c9b778fcf5cf5b42
Dan Jasnowski <djasnowski@gmail.com>
2017-12-31 17:25:13 -0600

    Add gzip and CSS sourcemaps

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false,
+    cssSourceMap: true,
   },
 
   build: {
@@ -69,7 +69,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
