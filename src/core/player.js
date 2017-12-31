a09c9840a506b27365b67e7c4dcea68f3ec8dc00
Dan Jasnowski <djasnowski@gmail.com>
2017-12-31 17:37:06 -0600

    Shoddy pathfinding (fix later)

--- src/core/player.js
+++ src/core/player.js
@@ -1,10 +1,12 @@
 import config from './config';
 import UI from './utilities/ui';
+import bus from '../core/utilities/bus';
 
 class Player {
   constructor(data) {
     this.x = data.x;
     this.y = data.y;
+    this.moving = false;
     this.surface_level = 1;
 
     this.server = 'us-1';
@@ -12,7 +14,11 @@ class Player {
     console.log(`[${this.server}] Player spawned at ${this.x}, ${this.y}`);
   }
 
-  move(direction, map) {
+  move(direction, map, pathfind = false) {
+    if (pathfind) {
+      this.moving = true;
+    }
+
     switch (direction) {
       default:
         console.log('Nothing happened');
@@ -48,29 +54,58 @@ class Player {
     map.drawPlayer();
   }
 
-  walkPath(path, map) {
-    setTimeout(() => {
-      const activePath = path;
+  walkPath(path, map, name) {
+    const speed = 250;
 
-      const steps = {
-        current: {
-          x: activePath.path[activePath.step][0],
-          y: activePath.path[activePath.step][1],
-        },
-        next: {
-          x: activePath.path[(activePath.step + 1)][0],
-          y: activePath.path[(activePath.step + 1)][1],
-        },
-      };
+    const onStep = {
+      last: path.path[path.path.length - 1],
+      loop: map.path.current.walking[map.path.current.walking.length - 1],
+    };
 
-      activePath.step += 1;
+    (() => {
+      setTimeout(() => {
+        if (!onStep.loop || JSON.stringify(onStep.last) === JSON.stringify(onStep.loop)) {
+          // If equal, it means our last step is the same as from
+          // when our pathfinding first started, so we keep going.
 
-      this.move(UI.getMovementDirection(steps), map);
+          const activePath = path;
+          if (JSON.stringify(activePath.path) !== JSON.stringify(map.path.current.walking)) {
+            bus.$emit('MAP:SET_PATH', activePath);
+          }
 
-      if ((activePath.step + 1) < activePath.length) {
-        this.walkPath(activePath, map);
-      }
-    }, 125);
+          const steps = {
+            current: {
+              x: activePath.path[0][0],
+              y: activePath.path[0][1],
+            },
+            next: {
+              x: activePath.path[1][0],
+              y: activePath.path[1][1],
+            },
+          };
+
+          activePath.step += 1;
+
+          console.log(name, activePath.step);
+
+          this.move(UI.getMovementDirection(steps), map, true);
+
+          if ((activePath.step + 1) < activePath.length) {
+            this.walkPath(activePath, map, name);
+            activePath.path.shift();
+          } else {
+            this.stopMovement();
+          }
+        } else {
+          debugger;
+        }
+      }, speed);
+    })(window);
+  }
+
+  stopMovement() {
+    this.moving = false;
+    bus.$emit('PLAYER:STOP_MOVEMENT');
   }
 
   /**
