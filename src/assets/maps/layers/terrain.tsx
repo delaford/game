7dd8b4bd9c78bfe2e65df47b5840fc27dc97152a
Dan Jasnowski <danjasnowski@gmail.com>
2018-01-15 21:17:45 -0600

    New game map assets.

---
+++ src/assets/maps/layers/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Terrain" tilewidth="32" tileheight="32" tilecount="234" columns="9">
+ <image source="../../tiles/terrain.png" width="288" height="832"/>
+</tileset>
