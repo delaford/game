76c861aa889c257fa6308350843114da930420f4
Dan Jasnowski <danjasnowski@gmail.com>
2018-02-24 23:04:05 -0600

    Actual Express configuration.

--- server.js
+++ server.js
@@ -6,9 +6,22 @@
  |_| \_|\__,_| \_/ \__,_|_|  |_|  \__,_|
 ******************************************/
 
+// Start Express
 const express = require('express');
-const server = express();
+const app = express();
 
+//Start Express Router
+const router = express.Router();
+const port = process.env.PORT || 8080;
+
+// Connect path to Express router
+app.use("/", router);
+app.use(express.static('static'))
+var server = app.listen(port, function () {
+  console.log('Node.js static server listening on port: ' + port)
+});
+
+// Actual game server
 const Navarra = require('./server/Navarra');
 require('dotenv').config();
 
@@ -17,7 +30,3 @@ const game = new Navarra(9000);
 
 // Start the game server.
 game.start();
-
-// Start the Express server
-server.use('/', express.static(__dirname + '/'));
-server.listen(7777);
