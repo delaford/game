61c4b1bfbfa51e7c5eccebe5e3c1990b0d096320
Dan Jasnowski <djasnowski@gmail.com>
2018-09-13 02:57:09 -0500

    43242

--- a.js
+++ a.js
@@ -1,3 +1,3 @@
-const webSocketServer = new (require('ws')).Server({port: (process.env.PORT || 5000)})
+const webSocketServer = new (require('ws')).Server({port: (5000)})
 
-console.log(`Logged on... ${process.env.PORT}`);
+console.log(webSocketServer);
