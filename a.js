f277f8865f44b3feef2a9978e2b5afc495c4db37
Dan Jasnowski <djasnowski@gmail.com>
2018-09-13 02:51:07 -0500

    123424

--- a.js
+++ a.js
@@ -1,15 +1,3 @@
-var express = require('express');
-var socketIO = require('ws');
-var path = require('path');
+const webSocketServer = new (require('ws')).Server({port: (process.env.PORT || 5000)})
 
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-var server =  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-var io = new socketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
+console.log(`Logged on... ${process.env.PORT}`);
