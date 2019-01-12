351e7d9a6b6cbf63e7450fc7af8143861138c13c
Dan Jasnowski <djasnowski@gmail.com>
2019-01-12 07:39:09 -0600

    Simple mining counter. WIP

--- server/core/skills/mining.js
+++ server/core/skills/mining.js
@@ -1,15 +1,36 @@
 import Skill from './index';
+import { foregroundObjects } from '../data/foreground';
 
 export default class Mining extends Skill {
-  constructor(playerId, rock) {
+  constructor(playerId, rockId) {
     super(playerId);
-    this.rock = rock;
+    this.rockId = rockId;
   }
 
+  get rock() {
+    return foregroundObjects.find(e => e.id === this.rockId);
+  }
+
+  /**
+   * Swing your pickaxe at a rock to mine
+   */
   pickAtRock() {
-    console.log(`Mining for ${this.rock}`);
+    let counter = 0;
+    console.log(`Mining for ${this.rock.resources}`);
+
+    const action = setInterval(() => {
+      counter += 1;
+      console.log('Picking at rock...');
+      if (counter === 3) {
+        clearInterval(action);
+        console.log('Done.');
+      }
+    }, 1000);
   }
 
+  /**
+   * Inspect a rock to be told what it is
+   */
   prospect() {
     const id = this.rock;
     console.log(id);
